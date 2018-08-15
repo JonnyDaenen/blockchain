e836f193b587b42c081907a6a5cd51ef877e5a0b
Jonny Daenen <jonnydaenen@gmail.com>
2018-08-15 14:32:49 +0200

    add block chain class skeleton

---
+++ blockchain.py
@@ -0,0 +1,29 @@
+
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new block and adds to chain
+        pass
+
+    def new_transaction(self):
+        # adds a new transaction to list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # returns the last block in the chain
+        pass
+
+
+
